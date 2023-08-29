4e9227edf7d4f7bd5b4577fbf800f891e5f2bdf9
ahmetsonmez <ahmet.sonmez37@gmail.com>
2023-08-30 01:43:24 +0300

    added partials views

--- blog-app/routes/user.js
+++ blog-app/routes/user.js
@@ -1,7 +1,27 @@
 const routers = require("express").Router();
 
 const data = {
-    "categories": ["Web Development","Mobile Development","Data Analysis"]
+    "categories": ["Web Development","Mobile Development","Data Analysis"],
+    "courses" : [
+        {
+            "Name" : "C#",
+            "Rating" : "4,5",
+            "Price" : "5$",
+            "Valid" : true
+        },
+        {
+            "Name" : "Python",
+            "Rating" : "5",
+            "Price" : "15$",
+            "Valid" : true
+        },
+        {
+            "Name" : "Javascript",
+            "Rating" : "4",
+            "Price" : "3$",
+            "Valid" : false
+        }
+    ]
 }
 
 routers.use("/blogs/:blogId",function(req,res){

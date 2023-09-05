3c51bf1e111fc80e1bc6b0955515f7c89462e1e7
ahmetsonmez <ahmet.sonmez37@gmail.com>
2023-09-06 00:22:32 +0300

    async await added

--- blog-app/routes/user.js
+++ blog-app/routes/user.js
@@ -1,46 +1,63 @@
 const routers = require("express").Router();
 const db = require("../data/db");
 
-const data = {
-    "categories": ["Web Development","Mobile Development","Data Analysis"],
-    "courses" : [
-        {
-            "Name" : "C#",
-            "Rating" : "4,5",
-            "Price" : "5$",
-            "Valid" : true
-        },
-        {
-            "Name" : "Python",
-            "Rating" : "5",
-            "Price" : "15$",
-            "Valid" : true
-        },
-        {
-            "Name" : "Javascript",
-            "Rating" : "4",
-            "Price" : "3$",
-            "Valid" : false
+routers.use("/blogs/:blogId", async function(req,res){
+    try {
+        const id = req.params.blogId;
+        const [blog, ] = await db.execute("select * from Blogs where IsValid =1 and Id=?",[id]);
+        
+        if(blog[0]){
+          return  res.render("users/details", {
+                blog : blog[0]
+            });  
         }
-    ]
-}
 
-routers.use("/blogs/:blogId",function(req,res){
-    res.render("users/details");   
+        return res.redirect("/");
+
+        
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 routers.use("/blogs",function(req,res){
     res.render("users/list");   
 });
 
-routers.use("/",function(req,res){
-    db.execute("select * from Blogs where IsValid =1")
-    .then(results => {
-            res.render("users/index",{
-                result : results[0]
-            });    
-        })
-        .catch(err => console.error(err));   
+routers.use("/", async function(req,res){
+
+    try {
+
+        const [blogs, ] = await db.execute("select * from Blogs where IsValid =1");
+        const [categories,] = await db.execute("select * from Categories");
+        
+        res.render("users/index",{
+            result : blogs,
+            categories : categories
+        }); 
+
+    } catch (error) {
+        console.error(error);
+    }
+
+
+    // If requested, it can also be used with a promise structure like this.
+    // However, the nested lines here make the code difficult to read. 
+    // Therefore, a better approach would be to use the async/await structure as above.
+
+    // db.execute("select * from Blogs where IsValid =1")
+    // .then(courseResult => {
+
+    //         db.execute("select * from Categories")
+    //         .then(categoryResult =>{
+    //             res.render("users/index",{
+    //                 result : courseResult[0],
+    //                 categories : categoryResult[0]
+    //             });  
+    //         })
+    //         .catch(err=> console.log(err))         
+    //     })
+    //     .catch(err => console.error(err));   
 });
 
 module.exports = routers;
